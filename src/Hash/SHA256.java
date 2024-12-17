8eeff884e8775da6ab1897b75f136f2de6b8d812
Eduardo Cerqueira <100794159+Eduardo-Cerqueira@users.noreply.github.com>
2024-12-17 23:04:01 +0100

    :sparkles: Implement SHA-256 hash generation from message

---
+++ src/Hash/SHA256.java
@@ -0,0 +1,30 @@
+package Hash;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    /**
+     * This function gives the hash of text using SHA-256 algorithm
+     * @param stringToHash Text to hash
+     * @return Hash for text
+     */
+    public String hash(String stringToHash) {
+        try {
+
+            MessageDigest MD5Instance = MessageDigest.getInstance("SHA-256");
+
+            byte[] messageDigest = MD5Instance.digest(stringToHash.getBytes(StandardCharsets.UTF_8));
+
+            BigInteger rawSHA256Hash = new BigInteger(1, messageDigest);
+
+            return rawSHA256Hash.toString(16);
+        }
+
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
