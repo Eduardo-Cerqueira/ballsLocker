6b70baea865588ede07c297ce059b7aa53251264
Eduardo Cerqueira <100794159+Eduardo-Cerqueira@users.noreply.github.com>
2024-12-17 16:18:24 +0100

    :sparkles: Add a function to generate a menu and manage user input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    static Menu menu = new Menu();
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        List<String> homeMenu = Arrays.asList("Crypter un message", "Decrypter un message");
+
+        int menuEntry = menu.generateMenu(homeMenu, "\nWhere do you want to go ?");
+
+        System.out.println("Menu entry " + menuEntry + " has been choosen !");
     }
 }
