0980e2dbafb2d663eff6e778a9260e9f45884ffb
Eduardo Cerqueira <100794159+Eduardo-Cerqueira@users.noreply.github.com>
2024-12-18 01:20:37 +0100

    :sparkles: Add function to validate message with hashsum

--- src/Helpers/Validation.java
+++ src/Helpers/Validation.java
@@ -4,4 +4,14 @@ public class Validation {
     public boolean isLowerCase(String word) {
         return word.equals(word.toLowerCase());
     }
+
+    /**
+     *
+     * @param stringToCompareWithHash Text to compare with hash
+     * @param hash Hash to match with text
+     * @return Boolean attesting the text integrity
+     */
+    public boolean compare(String stringToCompareWithHash, String hash) {
+        return hash(stringToCompareWithHash).equals(hash);
+    }
 }
