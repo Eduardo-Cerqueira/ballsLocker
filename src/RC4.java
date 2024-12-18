605ede77db7561a5ee5e369eecb1ed4378b4b543
rat-balls <delalande.ethan@gmail.com>
2024-12-18 12:47:37 +0100

    Added more comments, and fixed OOB index bugs.

--- src/RC4.java
+++ src/RC4.java
@@ -1,8 +1,9 @@
 public class RC4 {
-    public static byte[] encrypt(byte[] stringBytesToEncrypt, String encryptionKey) {
-        //Prevent the user from entering a key too small or too big, maybe handle keys too big later
-        if(encryptionKey.length() < 1 || encryptionKey.length() > 256) {
-            throw new IllegalArgumentException("key must be between 1 and 256 bytes");
+    public static byte[] encrypt(byte[] stringBytesToEncrypt, byte[] encryptionKeyBytes) {
+        //Prevent the user from entering a key too small. 
+        //A key too big will only have it's first 255 bytes taken into account.
+        if(encryptionKeyBytes.length < 1) {
+            throw new IllegalArgumentException("key must be more than 1 byte.");
         } else {
             //We initialize all our variables.
             //This will hold values from 0 to 255.
@@ -12,13 +13,13 @@ public class RC4 {
             //This will be our encryptedBytes and will be what we return.
             byte[] encryptedBytes = new byte[stringBytesToEncrypt.length];
             //These will be used as indexes during our loops.
-            int i, temp, swapIndex = 0;
+            int i, temp, XORcomparisonIndex, currByteIndex, swapIndex = 0;
 
             //We initialize S values from 0 to 255.
             //We initialize the extendedKeyBytes values by repeating the key bytes until we hit 255.
             for (i = 0; i < 256; i++) {
                 S[i] = i;
-                extendedKeyBytes[i] = encryptionKey.getBytes()[i % encryptionKey.length()];
+                extendedKeyBytes[i] = encryptionKeyBytes[i % encryptionKeyBytes.length];
             }
 
             //We scramble S by generating a swap index within a range of 255 based on our key bytes,
@@ -34,25 +35,28 @@ public class RC4 {
                 S[swapIndex] = temp;
             }
 
+            //This is the actual encryption process of the RC4 algorithm
             swapIndex = 0;
-            for(i = 0; i < stringBytesToEncrypt.length; i += 1 % 256) {
-                //Here we change the position of the swapIndex.
+            for(currByteIndex = 0; currByteIndex < stringBytesToEncrypt.length; currByteIndex++) {
+                i = (i + 1) % 256;
+                //Here we change the position of the swapIndex, we use it afterwards to swap values around in S and to generate the XORindex.
                 swapIndex = (swapIndex + S[i]) % 256;
                 //Here we swap S[i] and S[swapIndex] values.
                 temp = S[i];
                 S[i] = S[swapIndex];
                 S[swapIndex] = temp;
-                //Continue the RC4 algorithm ? Ask Clement if I need to understand/explain WHY we do this or just WHAT we are doing.
-                int XORcomparisonIndex = (S[i] + S[swapIndex]) % 256;
-                encryptedBytes[i] = (byte) (stringBytesToEncrypt[i] ^ S[XORcomparisonIndex]);
+                //Here we generate an index we will use afterwards during a XOR comparison.
+                XORcomparisonIndex = (S[currByteIndex] + S[swapIndex]) % 256;
+                //We compare the value from S[XORcomparisonIndex] to the current byte we are processing using a XOR operator.
+                encryptedBytes[currByteIndex] = (byte) (stringBytesToEncrypt[currByteIndex] ^ S[XORcomparisonIndex]);
             }
 
             return encryptedBytes;
         }
     }
 
-    public static byte[] decrypt(byte[] stringBytesToDecrypt, String decryptionKey) {
+    public static byte[] decrypt(byte[] stringBytesToDecrypt, byte[] decryptionKeyBytes) {
         //RC4 decryption process is the same as encryption process, so this is just for more intuitive use.
-        return encrypt(stringBytesToDecrypt, decryptionKey);
+        return encrypt(stringBytesToDecrypt, decryptionKeyBytes);
     }
 }
