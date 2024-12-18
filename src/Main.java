33e98e640bf53b29b2a408dcdb86258792fdf2b6
Eduardo Cerqueira <100794159+Eduardo-Cerqueira@users.noreply.github.com>
2024-12-18 12:03:57 +0100

    :sparkles: Add CipherBuilder to menu for chained encrypt/decrypt

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Cipher.Polybe;
 import Cipher.ROT;
 import Hash.MD5;
 import Hash.SHA256;
+import Helpers.CipherBuilder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,10 +25,12 @@ public class Main {
                 "Comparer un message avec un hash MD5",
                 "Comparer un message avec un hash SHA-256\n",
                 "Chiffrer un message avec Polybe",
-                "Déchiffrer un message avec Polybe"
+                "Déchiffrer un message avec Polybe\n",
+                "Chaine de encryptage",
+                "Chaine de decryptage"
         );
 
-        int menuEntry = Menu.generateMenu(homeMenu, "\nWhere do you want to go ?");
+        int menuEntry = Menu.generateMenu(homeMenu, "\nWhere do you want to go ?", homeMenu.size() + 1);
         System.out.println("Menu entry " + menuEntry + " has been choosen !");
 
         if (menuEntry == 1) {
@@ -67,6 +70,63 @@ public class Main {
         } else if (menuEntry == 8) {
             String word = inputString("Word to decrypt:", "Word is invalid !");
             System.out.println(polybe.decrypt(word));
+        } else if (menuEntry == 9) {
+            String word = inputString("Word to encrypt:", "Word is invalid !");
+            CipherBuilder cipherBuilder = new CipherBuilder(word);
+
+            boolean addAlgorithm = true;
+            while (addAlgorithm) {
+                List<String> algorithmsMenu = Arrays.asList("Ajouter du Rot encrypt", "Ajouter du Polybe encrypt");
+
+                int algorithmsMenuEntry = Menu.generateMenu(algorithmsMenu, "\nWhere do you want to go ?", algorithmsMenu.size() + 1);
+
+                switch (algorithmsMenuEntry) {
+                    case 1:
+                        int parsedInt = inputInteger("Number of rotate:", "Number is invalid !");
+                        cipherBuilder.encryptROT(parsedInt, true);
+                        System.out.println(cipherBuilder.getEncryptedMessage());
+                        break;
+                    case 2:
+                        cipherBuilder.encryptPolybe();
+                        System.out.println(cipherBuilder.getEncryptedMessage());
+                        break;
+                    default:
+                        addAlgorithm = false;
+                        break;
+                }
+            }
+
+            System.out.println(cipherBuilder.getEncryptedMessage());
+
+        }  else if (menuEntry == 10) {
+            String word = inputString("Word to decrypt:", "Word is invalid !");
+            CipherBuilder cipherBuilder = new CipherBuilder(word);
+
+            boolean addAlgorithm = true;
+            while (addAlgorithm) {
+                List<String> algorithmsMenu = Arrays.asList("Ajouter du Rot decrypt", "Ajouter du Polybe decrypt");
+
+                int algorithmsMenuEntry = Menu.generateMenu(algorithmsMenu, "\nWhere do you want to go ?", algorithmsMenu.size() + 1);
+
+                switch (algorithmsMenuEntry) {
+                    case 1:
+                        int parsedInt = inputInteger("Number of rotate:", "Number is invalid !");
+                        cipherBuilder.decryptROT(parsedInt);
+                        System.out.println(cipherBuilder.getEncryptedMessage());
+                        break;
+                    case 2:
+                        cipherBuilder.decryptPolybe();
+                        System.out.println(cipherBuilder.getEncryptedMessage());
+                        break;
+                    default:
+                        addAlgorithm = false;
+                        break;
+                }
+            }
+
+            System.out.println(cipherBuilder.getEncryptedMessage());
+        } else {
+            System.exit(0);
         }
     }
 }
